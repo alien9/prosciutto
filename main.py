39cc4da83ce5fed9910d6a6a87cd6caa073d4824
Tiago Barufi <tiago@homembala.com.br>
2015-02-23 19:51:07 -0300

    database no local correto

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ mail.init_app(app)
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect("ham.db", timeout=300) #connect_to_database()
+        db = g._database = sqlite3.connect("database/ham.db", timeout=300) #connect_to_database()
     return db
 
 @app.route('/')
